79447d77896b323846e1cbbede58bf7b6d2ef322
Pratik Kumar <pratikumar56@gmail.com>
2025-02-26 18:09:23 +0530

    changes made

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3002;
+const initializedb = require('shared_library/init');
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Salon Admin App');
 });
 
-app.listen(port, () => {
-  console.log(`Admin app running on port ${port}`);
+initializedb().then(() => {
+  app.listen(port, () => {
+    console.log(`Admin app running on port ${port}`);
+  });
 });
